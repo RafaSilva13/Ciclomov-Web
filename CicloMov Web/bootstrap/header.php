3dfe307ab62206ca62ceac8fe921059de588fa0e
LuzinGs <118901207+LuzinGs@users.noreply.github.com>
2022-12-07 11:39:54 -0300

    Home

--- CicloMov Web/bootstrap/header.php
+++ CicloMov Web/bootstrap/header.php
@@ -7,13 +7,41 @@
   <title>Ciclomov</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+  <style>
+    body{
+      background-color: rgb(247, 255, 251);
+    }
+
+    a{
+      text-decoration: none;
+    }
+
+    .progress-bar{
+      background-color: rgb(100, 200, 168);
+    }
+    
+    .progress{
+      height: 7px;
+    }
+
+    .card{
+      transition: all .2s ease-in-out;
+      box-shadow: 4px 4px 10px rgb(220, 220, 220);
+    }
+    .card:hover{
+      transform: scale(1.05);
+      box-shadow: 8px 8px 10px rgb(215, 215, 215);
+    }
+  </style>
+
 </head>
 
 <body>
   <!-- navbar  -->
   <nav class="navbar navbar-expand-lg " style="background-color: rgb(30, 170, 170);">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#"><img src="img/1168050.png" alt="" style="width:80px;"></a>
+      <a class="navbar-brand" href="#"><img src="img/logo.png" alt="" style="width:37px;"></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -23,7 +51,10 @@
             <a class="nav-link active" aria-current="page" href="index.php" style="color: white;">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" href="#" style="color: white;">Link</a>
+            <a class="nav-link active" aria-current="page" href="pontos.php" style="color: white;">Pontos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="sobre.php" style="color: white;">Sobre nós</a>
           </li>
         </ul>
         <form class="d-flex gap-4" role="search">
--- CicloMov Web/index.php
+++ CicloMov Web/index.php
@@ -1,44 +1,15 @@
 <?php
-include 'bootstrap/header.php';
-include 'conexao.php'; ?>
+    include 'bootstrap/header.php';
 
-<div class="container">
-    <div class="d-flex justify-content-center flex-wrap gap-3">
+?>
 
-        <?php
+<style>
+    body{
 
-        if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
-        }
-
-        $sql = "SELECT cod_ponto, vagas_livres_ponto, vagas_totais_ponto, descricao, mapa FROM pontos";
-        $result = mysqli_query($conn, $sql);
-
-        if (mysqli_num_rows($result) > 0) {
-            // output data of each row
-            while ($row = mysqli_fetch_assoc($result)) { ?>
-
-
-
-                <div class="card" style="width: 16rem; margin-top: 30px;">
-                    <iframe src="<?php echo $row['mapa']; ?>" style="border:0; whidt=100% " allowfullscreen="" loading="fast" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                    <div class="card-body">
-                        <h6 class="card-text" style="font-size: 20px; color: rgb(62, 122, 122); text-align: center;"><?php echo $row['descricao']; ?></h6>
-                    </div>
-                </div>
-
-
-
-        <?php }
-        } else {
-            echo "0 results";
-        }
-
-        mysqli_close($conn);
-        ?>
-    </div>
-</div>
+        background-image: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(30,170,170,1) 100%);        /* background-repeat: repeat-x; */
+    }
+</style>
 
 <?php
-include 'bootstrap/footer.php';
+    include 'bootstrap/footer.php';    
 ?>
