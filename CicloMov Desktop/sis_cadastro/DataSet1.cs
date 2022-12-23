8e8b317f9ac869d84050555c7dac6216ba2b5561
RafaelPS13 <rafa.silva@estudante.ufjf.br>
2022-12-23 17:31:52 -0300

    commit

--- CicloMov Desktop/sis_cadastro/DataSet1.cs
+++
@@ -1,9 +0,0 @@
-﻿using MySql.Data.MySqlClient;
-
-namespace CicloMov {
-    
-    
-    public partial class DataSet1 {
-         MySqlConnection cnn = new MySqlConnection("server=localhost;database=bd_estacionamento;uid=root;pwd=\"\";");
-    }
-}
