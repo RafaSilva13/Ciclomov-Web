9dc7552f481c6129de5403f29331838f7b56d2e5
Rafael Silva <117786730+RaffaPsilva@users.noreply.github.com>
2023-01-31 09:22:47 -0300

    commit

--- CicloMovWeb/ciclomov/application/views/view_navbar.php
+++ CicloMovWeb/ciclomov/application/views/view_navbar.php
@@ -4,16 +4,16 @@
         <div class="container-fluid">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item active">
-                    <a class="nav-link" href="http://localhost\CicloMov\CicloMovWeb\ciclomov/index.php/geral"><b>Home</b></a>
+                    <a class="nav-link item" href="http://localhost\CicloMov\CicloMovWeb\ciclomov/index.php/geral"><b>Home</b></a>
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link" href="http://localhost\CicloMov\CicloMovWeb\ciclomov/index.php/geral/pontos"><b>Pontos</b></a>
+                    <a class="nav-link item" href="http://localhost\CicloMov\CicloMovWeb\ciclomov/index.php/geral/pontos"><b>Pontos</b></a>
                 </li>
 
             </ul>
-            <form class="d-flex gap-4" role="search">
-                <a class="btn btn-light btn-sm btnlogin" href="login.php?t=l" type="button">Login</a>
+            <form class="d-flex gap-5">
+                <a class="btn btn-white btn-sm btnlogin" href="login.php?t=l" type="button">Login</a>
             </form>
         </div>
     </nav>
