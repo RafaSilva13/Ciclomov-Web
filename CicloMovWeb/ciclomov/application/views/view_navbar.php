948b2545b78f92cdd256d9ca5267389638bad12c
Rafael Silva <117786730+RaffaPsilva@users.noreply.github.com>
2023-01-31 09:13:28 -0300

    Commit

---
+++ CicloMovWeb/ciclomov/application/views/view_navbar.php
@@ -0,0 +1,20 @@
+<header>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <div class="container-fluid">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="http://localhost\rafael\a\CicloMov\CicloMov%20Web\ciclomov/index.php/geral"><b>Home</b></a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="http://localhost\rafael\a\CicloMov\CicloMov%20Web\ciclomov/index.php/geral/pontos"><b>Pontos</b></a>
+                </li>
+
+            </ul>
+            <form class="d-flex gap-4" role="search">
+                <a class="btn btn-light btn-sm btnlogin" href="login.php?t=l" type="button">Login</a>
+            </form>
+        </div>
+    </nav>
+
