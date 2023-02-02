de535790e93d49f5864ced52a260a6bef39a76fb
RafaelPS13 <rafa.silva@estudante.ufjf.br>
2023-02-02 01:53:09 -0300

    commit

--- CicloMov/ListarServiços.Designer.cs
+++ CicloMov/ListarServiços.Designer.cs
@@ -30,12 +30,188 @@
         {
             this.SuspendLayout();
             // 
+<<<<<<< Updated upstream
             // frmListarServiços
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(520, 337);
             this.Name = "frmListarServiços";
+=======
+            // dataGridView5
+            // 
+            this.dataGridView5.AllowUserToAddRows = false;
+            this.dataGridView5.AllowUserToDeleteRows = false;
+            this.dataGridView5.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView5.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView5.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView5.GridColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView5.Location = new System.Drawing.Point(0, 25);
+            this.dataGridView5.MultiSelect = false;
+            this.dataGridView5.Name = "dataGridView5";
+            this.dataGridView5.ReadOnly = true;
+            this.dataGridView5.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView5.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView5.RowHeadersVisible = false;
+            this.dataGridView5.Size = new System.Drawing.Size(460, 291);
+            this.dataGridView5.TabIndex = 10;
+            this.dataGridView5.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // barraClientes
+            // 
+            this.barraClientes.AddNewItem = null;
+            this.barraClientes.CountItem = null;
+            this.barraClientes.DeleteItem = null;
+            this.barraClientes.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.barraClientes.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripSeparator1,
+            this.toolStripButton2,
+            this.toolStripButton5,
+            this.toolStripSeparator4,
+            this.toolStripButton6});
+            this.barraClientes.Location = new System.Drawing.Point(0, 0);
+            this.barraClientes.Margin = new System.Windows.Forms.Padding(0, 5, 0, 0);
+            this.barraClientes.MoveFirstItem = null;
+            this.barraClientes.MoveLastItem = null;
+            this.barraClientes.MoveNextItem = null;
+            this.barraClientes.MovePreviousItem = null;
+            this.barraClientes.Name = "barraClientes";
+            this.barraClientes.Padding = new System.Windows.Forms.Padding(0, 1, 1, 0);
+            this.barraClientes.PositionItem = null;
+            this.barraClientes.Size = new System.Drawing.Size(460, 25);
+            this.barraClientes.TabIndex = 9;
+            this.barraClientes.Text = "bindingNavigator1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripLabel1.Margin = new System.Windows.Forms.Padding(10, 1, 0, 2);
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(59, 21);
+            this.toolStripLabel1.Text = "Serviços";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Margin = new System.Windows.Forms.Padding(2, 0, 0, 0);
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 24);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Margin = new System.Windows.Forms.Padding(1, 1, 1, 2);
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 21);
+            this.toolStripButton2.Text = "Recarregar";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButton5
+            // 
+            this.toolStripButton5.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripButton5.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.toolStripButton5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
+            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton5.Margin = new System.Windows.Forms.Padding(0, 1, 5, 2);
+            this.toolStripButton5.Name = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(23, 21);
+            this.toolStripButton5.Text = "toolStripButton5";
+            this.toolStripButton5.Click += new System.EventHandler(this.toolStripButton5_Click);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 24);
+            // 
+            // toolStripButton6
+            // 
+            this.toolStripButton6.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton6.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton6.Image")));
+            this.toolStripButton6.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton6.Margin = new System.Windows.Forms.Padding(5, 1, 5, 2);
+            this.toolStripButton6.Name = "toolStripButton6";
+            this.toolStripButton6.Size = new System.Drawing.Size(23, 21);
+            this.toolStripButton6.Text = "Menu";
+            this.toolStripButton6.Click += new System.EventHandler(this.toolStripButton6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(432, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Nome:";
+            // 
+            // txtPesquisarClientes
+            // 
+            this.txtPesquisarClientes.Location = new System.Drawing.Point(473, 3);
+            this.txtPesquisarClientes.Name = "txtPesquisarClientes";
+            this.txtPesquisarClientes.Size = new System.Drawing.Size(109, 20);
+            this.txtPesquisarClientes.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(325, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Código:";
+            // 
+            // txtPesquisarServicos
+            // 
+            this.txtPesquisarServicos.Location = new System.Drawing.Point(372, 3);
+            this.txtPesquisarServicos.Name = "txtPesquisarServicos";
+            this.txtPesquisarServicos.Size = new System.Drawing.Size(53, 20);
+            this.txtPesquisarServicos.TabIndex = 13;
+            this.txtPesquisarServicos.TextChanged += new System.EventHandler(this.txtPesquisarPontos_TextChanged);
+            // 
+            // frmListarServicos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 316);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPesquisarServicos);
+            this.Controls.Add(this.dataGridView5);
+            this.Controls.Add(this.barraClientes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPesquisarClientes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmListarServicos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+>>>>>>> Stashed changes
             this.Text = "Serviços";
             this.Load += new System.EventHandler(this.ListarServiços_Load);
             this.ResumeLayout(false);
--- CicloMov/ListarServiços.cs
+++ CicloMov/ListarServiços.cs
@@ -20,6 +20,229 @@ namespace CicloMov
         private void ListarServiços_Load(object sender, EventArgs e)
         {
 
+<<<<<<< Updated upstream
+=======
+            Tbl5.Columns.Add("N°", typeof(int));
+            Tbl5.Columns.Add("Código", typeof(int));
+            Tbl5.Columns.Add("Clientes", typeof(string));
+            Tbl5.Columns.Add("Ponto", typeof(string));
+            Tbl5.Columns.Add("Tipo de Serviço", typeof(string));
+            Tbl5.Columns.Add("Tempo Permanência", typeof(string));
+
+            MySqlConnection cnn = new MySqlConnection("datasource=127.0.0.1;port=3306;username=root;database=bd_estacionamento");
+            MySqlCommand comando = new MySqlCommand("SELECT * FROM servicos t1 INNER JOIN clientes t2 ON t1.id_clientes = t2.cod_clientes INNER JOIN pontos t3 ON t1.id_ponto = t3.cod_ponto", cnn);
+            try
+            {
+
+                cnn.Open();
+
+                MySqlDataReader myReader;
+                myReader = comando.ExecuteReader();
+                try
+                {
+                    while (myReader.Read())
+                    {
+                        int Max = dataGridView5.Rows.Count + 1;
+
+                        DataRow Linha;
+
+                        Linha = Tbl5.NewRow();
+
+                        Linha[0] = Max;
+                        Linha[1] = myReader.GetInt32(0);
+                        Linha[2] = myReader.GetString(11);
+                        Linha[3] = myReader.GetString(17);
+                        Linha[4] = myReader.GetString(3);
+                        Linha[5] = myReader.GetString(4);
+
+                        Tbl5.Rows.Add(Linha);
+
+                        dataGridView5.DataSource = Tbl5;
+
+                    }
+                }
+                finally
+                {
+                    myReader.Close();
+                    cnn.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            Tbl5.Clear();
+
+            MySqlConnection cnn = new MySqlConnection("datasource=127.0.0.1;port=3306;username=root;database=bd_estacionamento");
+            MySqlCommand comando = new MySqlCommand("SELECT * FROM servicos t1 INNER JOIN clientes t2 ON t1.id_clientes = t2.cod_clientes INNER JOIN pontos t3 ON t1.id_ponto = t3.cod_ponto", cnn);
+            try
+            {
+
+                cnn.Open();
+
+                MySqlDataReader myReader;
+                myReader = comando.ExecuteReader();
+                try
+                {
+                    while (myReader.Read())
+                    {
+                        int Max = dataGridView5.Rows.Count + 1;
+
+                        DataRow Linha;
+
+                        Linha = Tbl5.NewRow();
+
+                        Linha[0] = Max;
+                        Linha[1] = myReader.GetInt32(0);
+                        Linha[2] = myReader.GetString(11);
+                        Linha[3] = myReader.GetString(17);
+                        Linha[4] = myReader.GetString(3);
+                        Linha[5] = myReader.GetString(4);
+
+                        Tbl5.Rows.Add(Linha);
+
+                        dataGridView5.DataSource = Tbl5;
+
+                    }
+                }
+                finally
+                {
+                    myReader.Close();
+                    cnn.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void txtPesquisarPontos_TextChanged(object sender, EventArgs e)
+        {
+            if (txtPesquisarServicos.Text == "")
+            {
+                Tbl5.Clear();
+
+                MySqlConnection cnn = new MySqlConnection("datasource=127.0.0.1;port=3306;username=root;database=bd_estacionamento");
+                MySqlCommand comando = new MySqlCommand("SELECT * FROM servicos t1 INNER JOIN clientes t2 ON t1.id_clientes = t2.cod_clientes INNER JOIN pontos t3 ON t1.id_ponto = t3.cod_ponto", cnn);
+                try
+                {
+
+                    cnn.Open();
+
+                    MySqlDataReader myReader;
+                    myReader = comando.ExecuteReader();
+                    try
+                    {
+                        while (myReader.Read())
+                        {
+                            int Max = dataGridView5.Rows.Count + 1;
+
+                            DataRow Linha;
+
+                            Linha = Tbl5.NewRow();
+
+                            Linha[0] = Max;
+                            Linha[1] = myReader.GetInt32(0);
+                            Linha[2] = myReader.GetString(11);
+                            Linha[3] = myReader.GetString(17);
+                            Linha[4] = myReader.GetString(3);
+                            Linha[5] = myReader.GetString(4);
+
+                            Tbl5.Rows.Add(Linha);
+
+
+                            dataGridView5.DataSource = Tbl5;
+
+                        }
+                    }
+                    finally
+                    {
+                        myReader.Close();
+                        cnn.Close();
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void toolStripButton5_Click(object sender, EventArgs e)
+        {
+            MySqlConnection cnn = new MySqlConnection("datasource=127.0.0.1;port=3306;username=root;database=bd_estacionamento");
+            MySqlCommand comando = new MySqlCommand("SELECT * FROM servicos t1 INNER JOIN clientes t2 ON t1.id_clientes = t2.cod_clientes INNER JOIN pontos t3 ON t1.id_ponto = t3.cod_ponto WHERE cod_servico = @codServico", cnn);
+            try
+            {
+                comando.Parameters.AddWithValue("@codServico", txtPesquisarServicos.Text);
+                cnn.Open();
+
+                MySqlDataReader myReader;
+                myReader = comando.ExecuteReader();
+                try
+                {
+                    if (myReader.Read())
+                    {
+                        Tbl5.Clear();
+
+                        int Max = dataGridView5.Rows.Count + 1;
+
+                        DataRow Linha;
+
+                        Linha = Tbl5.NewRow();
+
+                        Linha[0] = Max;
+                        Linha[1] = myReader.GetInt32(0);
+                        Linha[2] = myReader.GetString(11);
+                        Linha[3] = myReader.GetString(17);
+                        Linha[4] = myReader.GetString(3);
+                        Linha[5] = myReader.GetString(4);
+
+                        Tbl5.Rows.Add(Linha);
+
+
+                        dataGridView5.DataSource = Tbl5;
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Código não encontrado!");
+                    }
+                }
+                finally
+                {
+                    myReader.Close();
+                    cnn.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void toolStripButton6_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            frmMenu area_menu = new frmMenu();
+
+            area_menu.Show();
+>>>>>>> Stashed changes
         }
     }
 }
