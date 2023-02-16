501184c0950af2bf7c9c2ba79431e3999a7b8356
Rafael Silva <117786730+RaffaPsilva@users.noreply.github.com>
2023-02-16 11:40:43 -0300

    commit

--- CicloMovWeb/ciclomov/application/models/Model_servico.php
+++ CicloMovWeb/ciclomov/application/models/Model_servico.php
@@ -8,10 +8,28 @@ class Model_servico extends CI_Model {
             "id_clientes" => $_SESSION['user'],
             "id_ponto" => $id_ponto,
             "tipo" => $tipo,
-            "tempo_permanencia" => $tempo
+            "tempo_permanencia" => $tempo,
+            "status" => 1
         );
 
         $this->db->insert('servicos',$dados);
+    }
+
+    public function verificarStatus($user)
+    {
+        $query = $this->db->get_where('servicos', array('id_clientes' => $user, 'status' => 1));
+
+        if ($query->result()) 
+        {            
+            foreach ($query->result() as $row)
+            {
+                return $row->tempo_permanencia;
+            }
+        } 
+        else 
+        {
+            return '#erro';
+        }
     }
 }
 
