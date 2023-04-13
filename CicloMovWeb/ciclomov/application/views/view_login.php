b6380c4488f0ca467cd15afed128abedc4361bc9
Rafael Silva <117786730+RaffaPsilva@users.noreply.github.com>
2023-04-13 15:36:56 -0300

    Revert "commit"
    This reverts commit 3017737ef7f9ea90a4a32a711af81078601318a2.

--- CicloMovWeb/ciclomov/application/views/view_login.php
+++ CicloMovWeb/ciclomov/application/views/view_login.php
@@ -43,18 +43,29 @@
             
                         <form id="formLogin">
 
-                            <div class="text-center mb-5" style="margin-top: 2rem; margin-bottom: 2rem;">
-                              
-                                <h2>Login</h2>
+                            <div class="text-center mb-3">
+                                <p>Entre com</p>
+                                
+                                <a class="btn text-white btn-floating mx-1" style="background-color: #3b5998;" href="#!" role="button">
+                                    <i class="fab fa-facebook-f"></i>
+                                </a>
+
+                                <a class="btn text-white btn-floating mx-1" style="background-color: #dd4b39;" href="#!" role="button">
+                                    <i class="fab fa-google"></i>
+                                </a>
 
                             </div>
+
+                            <p class="text-center">Ou</p>
                             
                             <!-- Email input -->
                             <div class="form-outline mb-3">
 
-                                <input type="email" id="loginName" name="usuario" class="form-control" required/>
+                                <input type="email" id="loginName" name="usuario" class="form-control" required>
 
-                                <label class="form-label" for="loginName">Email</label>
+                                <label class="form-label" for="loginName" style="margin-left: 0px;">Email</label>
+
+                                <div class="invalid-feedback">Campo vazio!</div>
 
                                 <div class="form-notch">
 
@@ -73,7 +84,9 @@
 
                                 <input type="password" id="senha" name="senha" class="form-control" required>
 
-                                <label class="form-label" for="senha">Senha</label>
+                                <label class="form-label" for="senha" style="margin-left: 0px;">Senha</label>
+
+                                <div class="invalid-feedback">Campo vazio!</div>
 
                                 <div class="form-notch">
 
@@ -114,6 +127,13 @@
                             <!-- Submit button -->
                             <button type="submit" class="btn btn-primary btn-block mb-4" id="btnEntrarLogin">Entrar</button>
 
+                            <!-- Register buttons -->
+                            <div class="text-center">
+
+                                <p class="mb-1">Não lembra a senha? Fale com o <a href="#!">Suporte</a></p>
+
+                            </div>
+
                         </form>
 
                     </div>
@@ -122,19 +142,27 @@
 
                         <form id="formCadastro">
                             
-                            
-                            <div class="text-center mb-4" style="margin-top: 2rem; margin-bottom: 2rem;">
-                              
-                                <h2>Cadastro</h2>
+                            <div class="text-center mb-3">
+                                <p>Cadastre-se com</p>
+                                
+                                <a class="btn text-white btn-floating mx-1" style="background-color: #3b5998;" href="#!" role="button">
+                                    <i class="fab fa-facebook-f"></i>
+                                </a>
+
+                                <a class="btn text-white btn-floating mx-1" style="background-color: #dd4b39;" href="#!" role="button">
+                                    <i class="fab fa-google"></i>
+                                </a>
 
                             </div>
+
+                            <p class="text-center">Ou</p>
 
                             <!-- Name input -->
                             <div class="form-outline mb-3">
                                 
-                                <input type="text" id="registerName" name="cad_nome_completo" class="form-control" required/>
+                                <input type="text" id="registerName" name="cad_nome_completo" class="form-control" required>
                                 
-                                <label class="form-label" for="registerName">Nome Completo</label>
+                                <label class="form-label" for="registerName" style="margin-left: 0px;">Nome Completo</label>
                                 
                                 <div class="form-notch">
 
@@ -154,9 +182,9 @@
 
                                     <div class="form-outline mb-3">
 
-                                        <input type="text" id="cpf" name="cad_cpf" class="form-control cpf-mask" required/>
+                                        <input type="text" id="cpf" name="cad_cpf" class="form-control cpf-mask" required>
                                         
-                                        <label class="form-label" for="cpf">CPF</label>
+                                        <label class="form-label" for="cpf" style="margin-left: 0px;">CPF</label>
                                         
                                         <div class="form-notch">
 
@@ -176,9 +204,9 @@
 
                                     <div class="form-outline mb-3">
 
-                                        <input type="text" id="telefone" name="cad_telefone" class="form-control cel-sp-mask" required/>
+                                        <input type="text" id="telefone" name="cad_telefone" class="form-control cel-sp-mask" required>
                                         
-                                        <label class="form-label" for="telefone">Telefone</label>
+                                        <label class="form-label" for="telefone" style="margin-left: 0px;">Telefone</label>
                                     
                                         <div class="form-notch">
 
@@ -201,7 +229,7 @@
                                 
                                 <input type="text" id="registerUsername" name="cad_usuario" class="form-control" required>
                                     
-                                <label class="form-label" for="registerUsername">Usuário</label>
+                                <label class="form-label" for="registerUsername" style="margin-left: 0px;">Usuário</label>
                                 
                                 <div class="form-notch">
 
@@ -220,7 +248,7 @@
                                 
                                 <input type="email" id="email" name="cad_email" class="form-control" required>
                                 
-                                <label class="form-label" for="email">Email</label>
+                                <label class="form-label" for="email" style="margin-left: 0px;">Email</label>
                                 
                                 <div class="form-notch">
                                     
@@ -242,7 +270,7 @@
                                     
                                         <input type="password" id="senha2" name="cad_senha" class="form-control" required>
                                     
-                                        <label class="form-label" for="senha2">Senha</label>
+                                        <label class="form-label" for="senha2" style="margin-left: 0px;">Senha</label>
                                         
                                         <div class="form-notch">
                                         
@@ -263,7 +291,7 @@
 
                                         <input type="password" id="senha3" class="form-control" required>
                                         
-                                        <label class="form-label" for="senha3">Repita a senha</label>
+                                        <label class="form-label" for="senha3" style="margin-left: 0px;">Repita a senha</label>
                                         
                                         <div class="form-notch">
 
