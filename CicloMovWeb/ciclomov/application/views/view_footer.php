867cfce8d6e0e0d53557128c80268ffcd0b83e66
Rafael Silva <117786730+RaffaPsilva@users.noreply.github.com>
2023-04-13 15:36:40 -0300

    Revert "commit"
    This reverts commit 3f8b95338a187c7d69cc9a33ff1c95d0e6fb39d8.

---
+++ CicloMovWeb/ciclomov/application/views/view_footer.php
@@ -0,0 +1,278 @@
+    <script>
+        $(function() {
+
+            $("#formLogin").submit(function(e){
+                e.preventDefault();
+                                    
+                var formData = new FormData(this);
+                
+                $.ajax({
+                    url: "<?php echo site_url("Geral/login")?>",
+                    type: 'POST',
+                    data: formData,
+                    success:  function(data){
+                        if(data == 'SenhaUserIn')
+                        {
+                            $(".erroToast").toast('show');
+                        }
+                        else
+                        {
+                            $('.btnlogin').hide();
+                            $(".AreaSelecaoPonto").removeClass("disabled");
+                            $('.trava').hide();
+                            $('.msgAviso').hide();
+                            $('#textoHome').hide();
+                            $('#btnPrincHome').hide();
+                            $('#btnSegundHome').show();
+                            $('#btnsaida').show();
+                            $('#areaMenu').show();
+
+                            $.ajax({
+                                url: "<?php echo site_url("Geral/pegarImagem")?>",
+                                type: 'POST',
+                                cache: false,
+                                contentType: false,
+                                processData: false
+                            }); 
+
+                            location.reload('hard');
+
+                        }
+                    },
+                    cache: false,
+                    contentType: false,
+                    processData: false
+                }); 
+            });
+            
+            $("#formEnviarImg").submit(function(e){
+                e.preventDefault();
+
+                var fileInput = document.getElementById('arquivo_para_upload');   
+                var filename = fileInput.files[0].name;
+
+                var formData = new FormData(this);
+                formData.append("nome", filename);
+
+                $.ajax({
+                    url: "<?php echo site_url("Geral/atualizarImagens")?>",
+                    type: 'POST',
+                    data: formData,
+                    success: function(data){
+                        console.log(data);
+
+                        $.ajax({
+                            url: "<?php echo site_url("Geral/pegarImagem")?>",
+                            type: 'POST',
+                            success: function(data){
+                                location.reload(true);
+                            },
+                            cache: false,
+                            contentType: false,
+                            processData: false
+                        }); 
+                    },
+                    cache: false,
+                    processData: false,
+                    contentType: false,
+                });
+            });
+
+            $("#formCadastro").submit(function(e){
+                e.preventDefault();
+
+                var formData = new FormData(this);
+
+                $.ajax({
+                    url: "<?php echo site_url("Geral/cadastro")?>",
+                    type: 'POST',
+                    data: formData,
+                    success: function(data){
+                        if(data == '1')
+                        {
+                            $(".cadastroToast").toast('show');
+                            
+                            $("#registerName").val("");
+                            $("#cpf").val("");
+                            $("#telefone").val("");
+                            $("#registerUsername").val("");
+                            $("#email").val("");
+                            $("#senha2").val("");
+                            $("#senha3").val("");
+                        }
+                        else
+                        {
+                            $(".cadErroToast").toast('show');
+                        }
+                    },
+                    cache: false,
+                    contentType: false,
+                    processData: false
+                }); 
+            });
+
+            <?php if(isset($_SESSION['user'])) { ?>                
+                $('.btnlogin').hide();
+                $(".AreaSelecaoPonto").removeClass("disabled");
+                $('.trava').hide();
+                $('.msgAviso').hide();
+                $('#btnsaida').show();
+                $('#areaMenu').show();
+                $('#textoHome').hide();
+                $('#btnPrincHome').hide();
+                $('#btnSegundHome').show();
+
+                $.ajax({
+                    url: "<?php echo site_url("Geral/verificarStatusServicos");?>",
+                    type: 'POST',
+                    success: function(data){    
+
+                        if(data == '0')
+                        {
+                            $('#btnTempo').hide();
+                            $('#textoTempo').hide();
+                        }
+                        else
+                        {
+                            $('#btnTempo').show();
+                            $('#textoTempo').show();
+                            $(".AreaSelecaoPonto").addClass("disabled");
+
+                            var countDownDate = new Date(data).getTime();
+            
+                            var x = setInterval(function() {
+
+                                var now = new Date().getTime();
+
+                                var distance = countDownDate - now;
+
+                                var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                                var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+                                if(minutes < 10) {
+                                    minutesMod = '0' + minutes;
+                                }
+                                else {
+                                    minutesMod = minutes;
+                                }
+
+                                if(seconds < 10) {
+                                    secundsMod = '0' + seconds;
+                                }
+                                else {
+                                    secundsMod = seconds;
+                                }
+
+                                document.getElementById("timer").innerHTML = minutesMod + ":" + secundsMod;
+
+                                if (distance < 0) {
+                                    clearInterval(x);
+                                    
+                                    document.getElementById("timer").innerHTML = "EXPIRADO";
+
+                                    $.ajax({
+                                        url: "<?php echo site_url("Geral/terminarServico")?>",
+                                        type: 'POST',
+                                        success: function(data){
+                                            location.reload(true);
+                                        },
+                                        cache: false,
+                                        contentType: false,
+                                        processData: false,
+                                        async: false 
+                                    });
+                                }
+
+                            }, 1000);
+                        }
+                    },
+                    cache: false,
+                    contentType: false,
+                    processData: false,
+                    async: false 
+                });     
+
+                // $("#modalLoginCadastro").hidden.bs.toast();
+            <?php } ?>
+            <?php if(!isset($_SESSION['user'])) { ?>
+
+                $('#btnTempo').hide();
+                $('#textoTempo').hide();
+                $('#btnsaida').hide();
+                $('#areaMenu').hide();
+                $('#textoHome').show();
+                $('#btnPrincHome').show();
+                $('#btnSegundHome').hide();
+
+            <?php } ?>
+
+            $('#btnSair').click(function(){
+
+                $.ajax({
+                    url: "<?php echo site_url("Geral/sair")?>",
+                    type: 'POST',
+                    success: function(){
+                        console.log('Saida efetuada!');
+                        location.reload(true);
+                    },
+                    cache: false,
+                    contentType: false,
+                    processData: false
+                }); 
+            });
+
+
+            $('#btnProximo').click(function() {
+                tipo = $('#selectServico').val();
+                tempo = $('#inputValor').val();
+
+                $('#time').html(tempo);
+                $('#service').html(tipo);
+                $('#point').html(id_ponto);
+
+            });
+
+            $('#confirmaServico').click(function() {
+                $.ajax({
+                    url: "<?php echo site_url("Geral/servico")?>",
+                    type: 'POST',
+                    data : { idPonto: id_ponto, tempo: tempo, tipo: tipo },
+                    success: function(){
+                        location.reload(true);
+                    },
+                    cache: false
+                }); 
+            });
+    
+        });
+        
+        function mudarValorTempo(tempo) {
+            $("#inputValor").val(tempo);
+        }
+
+        function mudarTipoServico(tempo) {
+            $("#selectServico").val(tempo);
+        }
+
+        function get_id(id_p, id_c) {
+            id_ponto = id_p;
+            id_cliente = id_c;
+        }
+    </script>
+
+    <!-- LocalWeb -->
+    <script type="text/javascript" src="//assets.locaweb.com.br/locastyle/2.0.6/javascripts/locastyle.js"></script>
+
+    <!-- Link script -->
+    <script src="<?= base_url('assets/js/script.js');?>"></script>
+
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+
+    <!-- MDB -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.js"></script>    
+    
+</body>
+</html>
---
+++ CicloMovWeb/ciclomov/application/views/view_modal.php
@@ -0,0 +1,219 @@
+<div class="modal fade" id="exampleCentralModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-modal="true" role="dialog" data-gtm-vis-recent-on-screen-2340190_1302="399562" data-gtm-vis-first-on-screen-2340190_1302="399562" data-gtm-vis-total-visible-time-2340190_1302="100" data-gtm-vis-has-fired-2340190_1302="1">
+
+    <div class="modal-dialog modal-sm">
+
+        <div class="modal-content text-center">
+
+            <div class="modal-header bg-danger text-white d-flex justify-content-center">
+
+                <h5 class="modal-title" id="exampleModalLabel">Deseja realmente sair?</h5>
+
+            </div>
+
+            <div class="modal-footer d-flex justify-content-center">
+
+                <button type="button" class="btn btn-outline-danger" data-mdb-dismiss="modal">Não</button>
+
+                <button type="button" class="btn btn-danger" data-mdb-dismiss="modal" id="btnSair">Sim</button>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="modalAreaImgUser" tabindex="-1" aria-labelledby="modalAreaImgUserLabel" aria-hidden="true">
+
+    <div class="modal-dialog">
+
+        <div class="modal-content">
+
+            <div class="modal-header">
+
+                <h5 class="modal-title" id="modalAreaImgUserLabel">Foto de Perfil</h5>
+                <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
+
+            </div>
+
+            <form id="formEnviarImg"  method="POST" enctype="multipart/form-data">
+
+                <div class="modal-body">
+                    
+                    <center>
+                    
+                        <div class="bg-image image-upload">
+                            
+                            <img src="<?= base_url('assets/images/'. $GLOBALS["imagem"])?>" id="fotoPerfil" class="figure-img img-fluid file-upload-image rounded-circle" style="width: 280px; height: 280px; margin-bottom: 1rem;">
+                            
+                        </div>
+                        
+                        <div class="form-outline mb-4 btn btn-primary" style="border-radius: 20px; width: 350px; height: 3rem; padding-top: 0; padding-bottom: 0; padding-left: 0; padding-right: 0; margin-top: 1rem; color: white">
+                        
+                            <label for='arquivo_para_upload' class="labelImg">Selecionar um arquivo</label>
+                            <input type="file" onchange="readURL(this);" class="form-control-lg text-white inputFile" name="arquivo_para_upload" id="arquivo_para_upload" id="formFileLg" style="border-radius: 20px; color: white; height: 3rem;"/>
+                            
+                        </div>
+
+                    </center>
+                
+                </div>
+                
+                <div class="modal-footer">
+                    
+                    <button type="button" class="btn btn-secondary" id="removeUpload" data-mdb-dismiss="modal">Fechar</button>
+                    <button type="submit" class="btn btn-primary">Salvar imagem</button>
+                    
+                </div>
+
+            </form>
+
+        </div>
+
+    </div>
+
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="modalHistorico" tabindex="-1" aria-labelledby="modalHistoricoLabel" aria-hidden="true">
+
+    <div class="modal-dialog">
+
+        <div class="modal-content">
+
+            <div class="modal-header">
+
+                <h5 class="modal-title" id="modalHistoricoLabel">Historico de Serviços</h5>
+                <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
+
+            </div>
+
+            <div class="modal-body" style="padding: 0;">
+
+                <table class="table align-middle mb-0 bg-white">
+
+                    <thead class="bg-light">
+
+                        <tr>
+
+                            <th>Ponto</th>
+                            <th>Tipo</th>
+                            <th>Status</th>
+                            <th>Tempo</th>
+
+                        </tr>
+
+                    </thead>
+
+                    <tbody id="tabela">
+
+                    </tbody>
+
+                </table>
+
+            </div>
+
+            <div class="modal-footer">
+
+                <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">Fechar</button>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<script>
+    $(function() {
+
+        $("#btnModalHistorico").click(function() {
+
+            $("#tabela tr").remove();
+
+            $.ajax({
+                url: '<?php echo site_url('Geral/listarServ'); ?>',
+                type: 'GET',
+                success: function(dados) {
+
+                    variavelphp = JSON.parse(dados);
+
+                    variavelphp.forEach(function(value, key) {
+
+                        $('#tabela').append("<tr><td style='padding: 5px;'><div class='d-flex align-items-center'><div class='ms-1'><p class='fw-bold mb-1'>" + value.descricaoPonto + "</p></div></div></td><td style='padding: 5px;'><p class='fw-normal mb-1' id='tipoServico" + value.codServ + "'>" + value.tipoServ + "</p></td><td style='padding: 5px;'><span class='badge badge-" + value.tipoMsg + " rounded-pill d-inline'" + value.codServ + "'>" + value.statusServ + "</span></td><td style='padding: 5px;'>" + value.valorTempoPerma + " minutos </td></tr>");
+
+                    });
+
+                },
+                cache: false,
+                contentType: false,
+                processData: false
+            });
+
+        });
+
+    });
+</script>
+
+<style>
+    .file-upload {
+        background-color: #ffffff;
+        width: 600px;
+        margin: 0 auto;
+        padding: 20px;
+    }
+
+    .file-upload-btn:hover {
+        background: #1AA059;
+        color: #ffffff;
+        transition: all .2s ease;
+        cursor: pointer;
+    }
+
+    .file-upload-btn:active {
+        border: 0;
+        transition: all .2s ease;
+    }
+
+    .file-upload-content {
+        display: none;
+        text-align: center;
+    }
+
+    .file-upload-input {
+        position: absolute;
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 100%;
+        outline: none;
+        opacity: 0;
+        cursor: pointer;
+    }
+
+    .image-upload-wrap {
+        margin-top: 20px;
+        position: relative;
+        transition: 0.25s;
+    }
+
+    .image-dropping,
+    .image-upload-wrap:hover {
+        background-color: rgb(235, 235, 235);
+        transition: 0.25s;
+    }
+
+    .drag-text {
+        text-align: center;
+    }
+
+    .drag-text h3 {
+        font-weight: 100;
+        text-transform: uppercase;
+        color: #15824B;
+        padding: 60px 0;
+    }
+</style>
